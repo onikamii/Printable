7dd0579ce3f696c3c194f990676e7af73b835cc9
User <User@DESKTOP-SNB119S>
2023-02-06 22:45:43 +0300

    Добавьте файлы проекта.

---
+++ Printable/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Printable
+{
+    interface Printable
+    {
+        void print();
+    }
+    class Book: Printable
+    {
+        public void print()
+        {
+            
+        }
+        static void printBooks(Printable[] printable)
+        {
+            Console.WriteLine(printable);
+        }
+    }
+    class Magazine : Printable
+    {
+        public void print()
+        {
+            
+        }
+        static void printMagazines(Printable[] printable)
+        {
+            Console.WriteLine(printable);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Magazine magazine = new Magazine();
+            Printable[] massiv = { };
+            foreach (Printable i in massiv)
+            {
+                book.print();
+                magazine.print();
+            }
+        }
+    }
+}
