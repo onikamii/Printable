3032b3ac96cb027d3859cc4e2e2ca7b5f84089a4
onikamii <lessiralf@gmail.com>
2023-04-04 23:53:29 +0300

    Добавьте файлы проекта.

---
+++ Printable/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Printable
+{
+    interface Printable
+    {
+        void print();
+    }
+    class Book : Printable
+    {
+        string book;
+        public Book(string book)
+        {
+            this.book = book;
+        }
+        public void print()
+        {
+            Console.WriteLine(book);
+        }
+        static void printBooks(Printable[] printabl)
+        {
+
+        }
+    }
+    class Magazine : Printable
+    {
+        string magazine;
+        public Magazine(string magazine)
+        {
+            this.magazine = magazine;
+        }
+        public void print()
+        {
+            Console.WriteLine(magazine);
+        }
+        static void printMagazines(Printable[] printabl)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Повелитель тайн");
+            Book book2 = new Book("Магический Трон Арканы");
+            Magazine magazine1 = new Magazine("Человек-бензопила");
+            Magazine magazine2 = new Magazine("Человек-бензопила 2");
+            Printable[] literature = {book1, book2, magazine1, magazine2};
+            foreach (Printable element in literature)
+            {
+                element.print();
+            }
+        }
+    }
+}
